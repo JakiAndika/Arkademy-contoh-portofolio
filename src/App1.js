b0f0423ebd1a7df6d6afc5c5bd0335d87d3d9855
Mohamad Jaki Andika 80 <jakiandika19@gmail.com>
2019-11-30 14:25:04 +0700

    LatihanUjian

---
+++ src/App1.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+
+} from "react-router-dom"
+import Landing from "./landing";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Landing>
+
+            </Landing>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
+
+// function App() {
